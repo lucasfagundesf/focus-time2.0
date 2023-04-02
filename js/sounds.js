55c2ad44275f28d739d9860a9ed7edbeab15fcba
Lucas Fagundes <lucas_fagundesfranco@hotmail.com>
2023-04-02 19:25:24 -0300

    alterações

--- js/sounds.js
+++ js/sounds.js
@@ -3,7 +3,7 @@ export default function Sound(){
     const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
     const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
     const soundFlorest = new Audio("https://github.com/lucasfagundesf/focus-time2.0/blob/main/sounds/Floresta.wav?raw=true")
-    const soundRain = new Audio("https://github.com/lucasfagundesf/focus-time2.0/blob/main/sounds/Chuva.wav?raw=true")
+    const soundRain = new Audio("https://drive.google.com/file/d/1Ip8xBqAUJ-bty51Wz8JBtX_bWXCgA0P2?raw=true")
     const soundCoffeeShop = new Audio("https://github.com/lucasfagundesf/focus-time2.0/blob/main/sounds/Cafeteria.wav?raw=true")
     const soundFirePlace = new Audio("https://github.com/lucasfagundesf/focus-time2.0/blob/main/sounds/Lareira.wav?raw=true")
 
